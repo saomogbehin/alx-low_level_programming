185d908790eb33144d94815c357e62743c132789
saomogbehin <saomogbehin@gmail.com>
2022-10-18 14:30:30 -0700

    my c proga

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
-
 /**
-
- *main - print a line of codde using puts
+ *main -print a line of codde using puts
  *
- *Return: Always 0 (Success)
-
+ *Return: 0
  */
-
-
-
 int main(void)
-  
-{
-  
-  puts("\"Programming is like building a multilingual puzzle");
-  
-  return (0);
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
