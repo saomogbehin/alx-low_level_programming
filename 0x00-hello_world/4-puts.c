67e7ca9e7fa92829d0f15780a54535740035dbd5
saomogbehin <saomogbehin@gmail.com>
2022-10-18 14:09:43 -0700

    my first c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+
+ *main - print a line of codde using puts
+ *
+ *Return: Always 0 (Success)
+
+ */
+
+
+
+int main(void)
+  
+{
+  
+  puts("\"Programming is like building a multilingual puzzle");
+  
+  return (0);
+}
