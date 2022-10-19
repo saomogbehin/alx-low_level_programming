ae98053a9bd4105db798acf55ec375eb12de2871
saomogbehin <saomogbehin@gmail.com>
2022-10-19 10:26:59 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+/**
+ * main - Prints all the numbers of base 16 in lowercase
+ * Return: Always 0.
+ */
+int main(void)
+{
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
