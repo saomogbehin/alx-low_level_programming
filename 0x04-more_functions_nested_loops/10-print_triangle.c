bc64cb6db3f7d10074f8516ce0fef4dcb60a64cc
saomogbehin <saomogbehin@gmail.com>
2022-10-21 16:46:48 -0700

    est

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_triangle - check the code forents.
+* @size: Determines size of triangle
+* Return: Always 0.
+*/
+void print_triangle(int size)
+{
+int i, j, k;
+if (size <= 0)
+_putchar('\n');
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = size - 1; j > i; j--)
+_putchar(' ');
+for (k = 0; k < i + 1; k++)
+_putchar('#');
+_putchar('\n');
+}
+}
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_most_numbers - check the code .
+* Return: Always 0.
+*/
+void print_most_numbers(void)
+{
+int num;
+for (num = '0'; num <= '9'; num++)
+{
+if (num == '2' || num == '4')
+continue;
+else
+_putchar(num);
+}
+_putchar('\n');
+}
