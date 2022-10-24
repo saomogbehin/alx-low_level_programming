b32d13180f0fce80b3187fdddfa5182c9d8b8f1f
saomogbehin <saomogbehin@gmail.com>
2022-10-24 08:51:30 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,16 +5,14 @@
 */
 void puts_half(char *str)
 {
-int i, len, m;
-for (len = 0; str[len] != '\0'; len++)
-{
-}
-if ((len  % 2) == 0)
-m = (len / 2);
-else
-m = (len + 1) / 2;
-for (i = m; i < len; i++)
-{
+int i, n, len;
+len = 0;
+for (i = 0; str[i] != '\0'; i++)
+len++;
+n = (len / 2);
+if ((len % 2) == 1)
+n = ((len + 1) / 2);
+for (i = n; str[i] != '\0'; i++)
 _putchar(str[i]);
-}
 _putchar('\n');
+}
