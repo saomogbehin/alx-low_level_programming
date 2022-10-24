3e110a173dc7390e6b5c0e249a8dff1e8db10527
saomogbehin <saomogbehin@gmail.com>
2022-10-24 13:08:01 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,29 @@
-#include "main.h"
+#include "main.h
 /**
-* _atoi - Entry point
-* @s: input
-* Return: Always 0 (Success)
+* _atoi - converts a string to an integer.
+* @s: the string to convert
+* Return: the converted string.
 */
 int _atoi(char *s)
 {
-return (0);
+short boolean;
+int i, minus, result;
+i = minus = result = boolean = 0;
+minus = -1;
+while (s[i] != '\0')
+{
+if (s[i] == '-'
+minus *= -1;
+if (s[i] >= '0' && s[i] <= '9')
+{
+result *= 10;
+result -= (s[i] - '0');
+boolean = 1;
+}
+else if (boolean == 1)
+break;
+i++;
+}
+result *= minus;
+return (result);
+}
